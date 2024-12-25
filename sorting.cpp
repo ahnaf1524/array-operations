48a4187a47640fa27ef5425ca54b6c2dd42ed975
ahnaf1524 <ahnaf.tahmid.career@gmail.com>
2024-12-25 18:26:09 +0600

    Update sorting.cpp

--- sorting.cpp
+++ sorting.cpp
@@ -1,31 +1,62 @@
 #include <iostream>
 using namespace std;
-int main()
-{
-    // taking size and array elements from user
-    int n;
-    cin >> n;
-    int arr[n];
-    for (int i = 0; i < n; ++i)
-    {
-        cin >> arr[i];
-    }
-    for (int i = 0; i < n - 1; ++i)
-    {
-        for (int j = 0; j < n - i - 1; ++j)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+
+// Function to sort the array in ascending order
+void sortAscending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] > arr[j]) {
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
             }
         }
     }
-    cout << "Sorted array  : ";
-    for (int i = 0; i < n; ++i)
-    {
+}
+
+// Function to sort the array in descending order
+void sortDescending(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] < arr[j]) {
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+}
+
+// Function to print the array
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    // Sort in ascending order
+    sortAscending(arr, n);
+    cout << "Array in ascending order: ";
+    printArray(arr, n);
+
+    // Sort in descending order
+    sortDescending(arr, n);
+    cout << "Array in descending order: ";
+    printArray(arr, n);
+
     return 0;
 }
